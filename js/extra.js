1c2d6f77ce2944fd1e7c0c349dfaa60d29113921
Jeremy Ong <jeremycong@gmail.com>
2020-02-17 17:09:22 -0700

    Deployed ff37009 with MkDocs version: 1.0.4

--- js/extra.js
+++ js/extra.js
@@ -1,3 +1,4 @@
+/*
 window.MathJax = {
     options: {
         ignoreHtmlClass: 'tex2jax_ignore',
@@ -35,3 +36,42 @@ window.MathJax = {
         load: ['[tex]/noerrors']
     }
 };
+*/
+(function () {
+    'use strict';
+
+    var katexMath = (function () {
+        var maths = document.querySelectorAll('.arithmatex'),
+            tex;
+
+        for (var i = 0; i < maths.length; i++) {
+            tex = maths[i].textContent || maths[i].innerText;
+            if (tex.startsWith('\\(') && tex.endsWith('\\)')) {
+                katex.render(tex.slice(2, -2), maths[i], { 'displayMode': false });
+            } else if (tex.startsWith('\\[') && tex.endsWith('\\]')) {
+                katex.render(tex.slice(2, -2), maths[i], { 'displayMode': true });
+            }
+        }
+    });
+
+    (function () {
+        var onReady = function onReady(fn) {
+            if (document.addEventListener) {
+                document.addEventListener("DOMContentLoaded", fn);
+            } else {
+                document.attachEvent("onreadystatechange", function () {
+                    if (document.readyState === "interactive") {
+                        fn();
+                    }
+                });
+            }
+        };
+
+        onReady(function () {
+            if (typeof katex !== "undefined") {
+                katexMath();
+            }
+        });
+    })();
+
+}());
